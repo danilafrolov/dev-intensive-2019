399589f3ffc266502e4dd5334a7d6f8c35e5eb08
danila <frolovdanila1993@gmail.com>
2020-02-18 22:12:45 +0700

    hometask_4 CircleImageView and initials

--- app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/ui/profile/ProfileActivity.kt
@@ -15,6 +15,7 @@ import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_profile.*
 import ru.skillbranch.devintensive.R
 import ru.skillbranch.devintensive.models.Profile
+import ru.skillbranch.devintensive.utils.Utils
 import ru.skillbranch.devintensive.viewmodels.ProfileViewModel
 
 @Suppress("UNUSED_ANONYMOUS_PARAMETER")
@@ -60,7 +61,7 @@ class ProfileActivity : AppCompatActivity() {
                 v.text = it[k].toString()
             }
         }
-
+        iv_avatar.initials = Utils.toInitials(profile.firstName, profile.lastName)
     }
 
     private fun initViews(savedInstanceState: Bundle?) {
